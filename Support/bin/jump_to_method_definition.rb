55a41b7debe8b66921c505751472f0dc975bc2da
Josh Symonds <jsymonds@backstopsolutions.com>
2010-02-09 16:46:18 -0600

    Adding in a column summarizing what we've found

--- Support/bin/jump_to_method_definition.rb
+++ Support/bin/jump_to_method_definition.rb
@@ -26,7 +26,7 @@ def find_in_file(file, match_string)
   begin
     File.open(file) do |f|
       f.each_line do |line|
-        @found << {:file => f.path, :line => f.lineno} if line.match(match_string)
+        @found << {:filename => f.path, :line_number => f.lineno, :line => line.strip} if line.match(match_string)
       end
     end
   rescue Errno::ENOENT
@@ -55,16 +55,19 @@ end
 if @found.empty?
   TextMate.exit_show_tool_tip("Could not find definition for '#{@original_term}'")
 elsif @found.size == 1  
-  TextMate.open(File.join(@found[0][:file]), @found[0][:line] - 1)
-  TextMate.exit_show_tool_tip("Found definition for '#{@original_term}' in #{@found[0][:file]}")
+  TextMate.open(File.join(@found[0][:filename]), @found[0][:line_number] - 1)
+  TextMate.exit_show_tool_tip("Found definition for '#{@original_term}' in #{@found[0][:filename]}")
 else
   TextMate::HTMLOutput.show(
     :title      => "Definitions for #{@original_term}",
     :subtitle   => "#{@found.size} Definitions Found"
   ) do |io|
-    io << "<div class='executor'><table border='0' cellspacing='4' cellpading'0'><tbody>"
+    io << "<div class='executor'><table border='0' cellspacing='5' cellpading'0'>" 
+    io << "<thead><td><h4>Location</h4></td><td><h4>Line</h4></td><td><h4>Definition</h4></td></thead><tbody>"
     @found.each do |location|
-      io << "<tr><td><a class='near' href='txmt://open?url=file://#{location[:file]}&line=#{location[:line]}'>#{location[:file]}</a></td><td>line #{location[:line]}</td></tr>"
+      io << "<tr><td><a class='near' href='txmt://open?url=file://#{location[:filename]}&line=#{location[:line_number]}'>#{location[:filename]}</a></td>"
+      io << "<td>#{location[:line_number]}</td>"
+      io << "<td><strong>#{location[:line].gsub(/(def |named_scope \:|scope \:|has_many \:|has_one \:|belongs_to \:|has_and_belongs_to_many \:)/,'')}</strong><td?</tr>"
     end
     io << "</tbody></table></div>"
   end
