2029bd8424768e3deb0e14f6bc899329b087006e
Josh Symonds <jsymonds@backstopsolutions.com>
2010-02-09 14:37:29 -0600

    Cleaning things up a little bit

--- Support/bin/jump_to_method_definition.rb
+++ Support/bin/jump_to_method_definition.rb
@@ -32,20 +32,23 @@ def find_in_file(file, match_string)
   end
 end
 
-# First, if this is a route, we know this is in routes.rb
+# First, search the local project for any potentially matching method.
+find_in_file_or_directory(@root, "^\s*def #{@term}([\(]{1}[^\)]*[\)]{1}\s*$|\s*$)") 
+find_in_file_or_directory(@root, "^\s*(belongs_to|has_many|has_one|has_and_belongs_to_many|scope|named_scope) :#{@term}[\,]?")
+
+# Second, if this is a route, we know this is in routes.rb
 if path = @term.match(/(new_|edit_)?(.*?)_(path|url)/)
   path = path[2].split('_').first
   filename = File.join(@root,"config","routes.rb")
   find_in_file_or_directory(filename, "[^\.].resource[s]? (:|')#{path}(s|es)?[']?")
 end
 
-# Second, search the local project for any potentially matching method.
-find_in_file_or_directory(@root, "^\s*def #{@term}([\(]{1}[^\)]*[\)]{1}\s*$|\s*$)") 
-find_in_file_or_directory(@root, "^\s*(belongs_to|has_many|has_one|has_and_belongs_to_many|scope|named_scope) :#{@term}[\,]?")
 
 # Third, search the Gems directory, pulling only the most recent gems, but only if we haven't yet found a match.
-Gem.latest_load_paths.each do |directory|
-  find_in_file_or_directory(directory, "^\s*def #{@term}([\(]{1}[^\)]*[\)]{1}\s*$|\s*$)")
+if @found.empty?
+  Gem.latest_load_paths.each do |directory|
+    find_in_file_or_directory(directory, "^\s*def #{@term}([\(]{1}[^\)]*[\)]{1}\s*$|\s*$)")
+  end
 end
 
 # Render results sensibly.
@@ -56,7 +59,8 @@ elsif @found.size == 1
   TextMate.exit_show_tool_tip("Found definition for '#{@original_term}' in #{@found[0][:file]}")
 else
   TextMate::HTMLOutput.show(
-    :title      => "#{@found.size} Method Definitions Found"
+    :title      => "Definitions for #{@original_term}",
+    :subtitle   => "#{@found.size} Definitions Found"
   ) do |io|
     io << "<div class='executor'><table border='0' cellspacing='4' cellpading'0'><tbody>"
     @found.each do |location|
