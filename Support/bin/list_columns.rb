ad83312e7cbfae98840695a7303ffae7e493918d
Carlos Brando <eduardobrando@gmail.com>
2010-04-22 02:03:57 -0300

    Added associations in list_columns command menu.

--- Support/bin/list_columns.rb
+++ Support/bin/list_columns.rb
@@ -3,40 +3,45 @@
 require "yaml"
 require "#{ENV["TM_SUPPORT_PATH"]}/lib/ui"
 
-CACHE_DIR = File.expand_path("tmp/textmate/", ENV['TM_PROJECT_DIRECTORY'])
-CACHE_FILE = File.join(CACHE_DIR, "cache.yml")
-
 begin
+  PROJECT    = ENV['TM_PROJECT_DIRECTORY']
+  CACHE_DIR  = File.expand_path("tmp/textmate/", PROJECT)
+  CACHE_FILE = File.join(CACHE_DIR, "cache.yml")
+
   Dir.mkdir(CACHE_DIR) unless File.exists?(CACHE_DIR)
   cache = if File.exist?(CACHE_FILE)
     YAML.load(File.read(CACHE_FILE)) || {}
   else
     {}
   end
-  
-  word = ENV['TM_CURRENT_WORD'].scan(/\w*/).select { |x| !x.empty? }.last
-  
-	if word.nil? || word.empty?
-	  TextMate::UI.tool_tip("Place cursor on class name (or variation) to show its schema")
-	  exit
-	end
 
-  if cache[word] && cache["time_#{word}"] > (Time.now - 3600)
-    options = cache[word]
+  word = ENV['TM_CURRENT_WORD'].scan(/\w*/).select { |x| !x.empty? }.last
+
+  if word.nil? || word.empty?
+    TextMate::UI.tool_tip("Place cursor on class name (or variation) to show its schema")
+    exit
+  end
+
+  if cache[word] && cache[word][:expire_after] > Time.now
+    columns      = cache[word][:columns] || []
+    associations = cache[word][:associations] || []
+    
+    options = associations + ["---"] + columns
     selected = TextMate::UI.menu(options)
     STDOUT << options[selected] if selected
   else
-    require "#{ENV['TM_PROJECT_DIRECTORY']}/config/environment"
+    require "#{PROJECT}/config/environment"
 
     klass = word.camelcase.singularize.constantize rescue nil
     if klass and klass.class == Class and klass.ancestors.include?(ActiveRecord::Base)
-      columns = klass.columns_hash
-      options = columns.map { |col, attrs| col }.sort
+      columns      = klass.column_names
+      associations = klass.reflections.stringify_keys.keys
+      
+      cache[word] = { :associations => associations, :columns => columns, :expire_after => 1.hour.since }
 
-      cache["time_#{word}"] = Time.now
-      cache[word] = options
       File.open(CACHE_FILE, 'w') { |out| YAML.dump(cache, out ) }
 
+      options = associations + ["---"] + columns
       selected = TextMate::UI.menu(options)
       STDOUT << options[selected] if selected
 
