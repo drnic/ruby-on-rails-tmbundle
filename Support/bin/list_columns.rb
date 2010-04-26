8bd8756db7ff971cc197196ab6d4a10ba0d07337
Carlos Brando <eduardobrando@gmail.com>
2010-04-26 17:39:15 -0300

    Changed the way to store the database's columns in the cache.

--- Support/bin/list_columns.rb
+++ Support/bin/list_columns.rb
@@ -1,56 +1,84 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby -W0
 
+require 'rubygems'
 require "yaml"
 require "#{ENV["TM_SUPPORT_PATH"]}/lib/ui"
 
-begin
-  PROJECT    = ENV['TM_PROJECT_DIRECTORY']
-  CACHE_DIR  = File.expand_path("tmp/textmate/", PROJECT)
-  CACHE_FILE = File.join(CACHE_DIR, "cache.yml")
+PROJECT    = ENV['TM_PROJECT_DIRECTORY']
+CACHE_DIR  = File.expand_path("tmp/textmate/", PROJECT)
+CACHE_FILE = File.join(CACHE_DIR, "cache.yml")
 
-  Dir.mkdir(CACHE_DIR) unless File.exists?(CACHE_DIR)
-  cache = if File.exist?(CACHE_FILE)
-    YAML.load(File.read(CACHE_FILE)) || {}
-  else
-    {}
-  end
+def load_and_cache_all_models
+  begin
+    require "#{ENV['TM_SUPPORT_PATH']}/lib/progress"
+    cache = {}
 
-  word = ENV['TM_CURRENT_WORD'].scan(/\w*/).select { |x| !x.empty? }.last
+    File.delete(CACHE_FILE) if File.exists?(CACHE_FILE)
 
-  if word.nil? || word.empty?
-    TextMate::UI.tool_tip("Place cursor on class name (or variation) to show its schema")
-    exit
-  end
+    TextMate.call_with_progress(:title => "Contacting database", :message => "Fetching database schema…") do
+      require "#{PROJECT}/config/environment"
 
-  if cache[word] && cache[word][:expire_after] > Time.now
-    columns      = cache[word][:columns] || []
-    associations = cache[word][:associations] || []
-    
-    options = associations + ["---"] + columns
-    selected = TextMate::UI.menu(options)
-    STDOUT << options[selected] if selected
-  else
-    require "#{PROJECT}/config/environment"
-
-    klass = word.camelcase.singularize.constantize rescue nil
-    if klass and klass.class == Class and klass.ancestors.include?(ActiveRecord::Base)
-      columns      = klass.column_names
-      associations = klass.reflections.stringify_keys.keys
-      
-      cache[word] = { :associations => associations, :columns => columns, :expire_after => 1.hour.since }
-
-      File.open(CACHE_FILE, 'w') { |out| YAML.dump(cache, out ) }
-
-      options = associations + ["---"] + columns
-      selected = TextMate::UI.menu(options)
-      STDOUT << options[selected] if selected
-
-    elsif klass and klass.class == Class and not klass.ancestors.include?(ActiveRecord::Base)
-      TextMate::UI.tool_tip("'#{word}' is not an Active Record derived class")
-    else
-      TextMate::UI.tool_tip("'#{word}' was not recognised as a class")
+      Dir.glob(Rails.root.join("app/models/*.rb")) do |file|
+        klass = File.basename(file, '.*').camelize.constantize rescue nil
+        
+        if klass and klass.class == Class and klass.ancestors.include?(ActiveRecord::Base)
+          cache[klass.name.underscore] = { 
+            :associations => klass.reflections.stringify_keys.keys, 
+            :columns      => klass.column_names 
+          } rescue nil
+        end
+      end
     end
+    
+  rescue Exception => e
+    TextMate::UI.tool_tip(e.message)
+  ensure
+    File.open(CACHE_FILE, 'w') { |out| YAML.dump(cache, out ) }
+    return cache
   end
-rescue Exception => e
-  TextMate::UI.tool_tip(e.message)
 end
+
+def show_options
+  begin
+    Dir.mkdir(CACHE_DIR) unless File.exists?(CACHE_DIR)
+    cache = if File.exist?(CACHE_FILE)
+      # TODO: Verificar se houve alguma atualização
+      YAML.load(File.read(CACHE_FILE))
+    else
+      load_and_cache_all_models
+    end
+
+    word = ENV['TM_CURRENT_WORD'].scan(/\w*/).select { |x| !x.empty? }.last
+    if word.nil? || word.empty?
+      TextMate::UI.tool_tip("Place cursor on class name (or variation) to show its schema")
+      exit
+    end
+
+    require 'active_support/inflector'
+    klass = word.singularize.underscore
+
+    if cache[klass]
+      columns      = cache[klass][:columns]
+      associations = cache[klass][:associations]
+
+      options = associations + ["---"] + columns + ["---", "Reload..."]
+      selected = TextMate::UI.menu(options)
+      return if selected.nil?
+      
+      if options[selected] == "Reload..."
+        $".delete_if { |x| x.start_with?('active_support') }
+        load_and_cache_all_models
+        show_options
+      else
+        STDOUT << options[selected]
+      end
+    else
+      TextMate::UI.tool_tip("'#{klass}' is not an Active Record derived class or was not recognised as a class")
+    end
+
+  rescue Exception => e
+    TextMate::UI.tool_tip(e.message)
+  end
+end
+
+show_options
