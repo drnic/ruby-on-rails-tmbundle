0ac097006473cdfc641764dae6b6f65248cbb000
Carlos Brando <eduardobrando@gmail.com>
2010-05-14 23:57:28 -0300

    Fixed bugs when using RVM.

---
+++ Support/lib/rvm/choose_rvm.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require "rails_bundle_tools"
+
+RVM_MESSAGE = "To execute this command you need to set the default ruby environment for this project.\n(a project .rvmrc file will be created)\n\nChoose the default environment for this project: "
+
+# assemble a list of all RVM settings available
+ruby_versions = `rvm list strings`.split
+options = ruby_versions.collect do |ruby|
+  gemsets = `rvm #{ruby} gemset list`.split("\n")
+  gemsets.shift
+  gemsets.delete_if { |vg| vg == '*' }
+  
+  [ruby, gemsets.map { |g| "#{ruby}@#{g}" }]
+end.flatten
+
+# creates the .rvmrc file with the environment configuration chosen.
+if selected = TextMate.choose(RVM_MESSAGE, options, :title => "RVM environment")
+  File.open("#{TextMate.project_directory}/.rvmrc", 'w') {|f| f.write("rvm #{options[selected]}") }
+end
