7d5489d073b3a38086ca85b5d3bfc14aa53e9445
Carlos Brando <eduardobrando@gmail.com>
2010-05-27 15:58:32 -0300

    Refactoring 'Set RVM...' snippet.

--- Support/lib/rvm/check_rvm_version.rb
+++
@@ -1,7 +0,0 @@
-CURRENT_RVM_VERSION = "0.1.33"
-regex = /rvm (\d+\.\d+\.\d+)/
-
-if `rvm version`.scan(regex).flatten.first < CURRENT_RVM_VERSION
-  TextMate::UI.tool_tip("RVM is outdated. Update with 'rvm update'.")
-  exit
-end
---
+++ Support/lib/rvm/rvm.rb
@@ -0,0 +1,83 @@
+#!/usr/bin/env ruby
+
+require "rails_bundle_tools"
+require File.join(TextMate.support_path, "lib", "escape")
+require File.join(TextMate.support_path, "lib", "osx", "plist")
+
+module TextMate
+  class RVM
+    CURRENT_RVM_VERSION = "0.1.33"
+    OK = '1'
+    NEW_GEMSET = '3'
+    
+    def choose!(message = nil)
+      TextMate.exit_show_tool_tip("RVM is outdated. Update with 'rvm update'.") if outdated?
+      
+      button, selected = TextMate.standard_choose(message || formatted_message, gemsets, :title => "RVM Environment", :button3 => "Set a new gemset...")
+
+      case button
+      when OK
+        create_rvmrc(gemsets[selected.to_i])
+      when NEW_GEMSET
+        parameters = { 
+          "rubies"       => rubies, 
+          "selectedRuby" => gemsets[selected.to_i].split('@').first, 
+          'gemset'       => TextMate.project_directory.split('/').last
+        }
+        nib     = File.join(TextMate.bundle_support, "lib", "rvm", "nib", "SetGemset")
+        command = "#{e_sh ENV['DIALOG']} -cmp #{e_sh parameters.to_plist} #{e_sh(nib)}"
+
+        plist = OSX::PropertyList::load(`#{command}`)
+        if plist['result']
+          create_rvmrc "#{plist['selectedRuby']}@#{plist['result']['returnArgument']}"
+        end
+      end      
+    end
+    
+   private
+    def installed?
+      @installed ||= File.exists?("#{TextMate.project_directory}/.rvmrc")
+    end
+   
+    def version
+      @version ||= if installed?
+        rvm_version = File.open("#{TextMate.project_directory}/.rvmrc").read
+        rvm_version.sub('rvm use', '').strip
+      end
+      
+      return @version
+    end
+    
+    def outdated?
+      `rvm version`.scan(/rvm (\d+\.\d+\.\d+)/).flatten.first < CURRENT_RVM_VERSION
+    end
+    
+    def formatted_message
+      if installed? && !version.empty?
+        "This project is already configured to use #{version}\n\nChange the default environment for this project: "
+      else
+        "Choose the default environment for this project: "
+      end
+    end
+    
+    def rubies
+      @rubies ||= `rvm list strings`.split
+    end
+    
+    def gemsets
+      @gemsets ||= rubies.collect do |ruby|
+        gemsets = `rvm #{ruby} gemset list`.split("\n")
+        gemsets.reject! { |e| e.empty? || e =~ /^<i>/ }
+
+        [ruby, gemsets.map { |g| "#{ruby}@#{g}" }]
+      end.flatten
+    end
+    
+    def create_rvmrc(gemset)
+      `(cd #{TextMate.project_directory}; rvm --create --rvmrc use #{gemset}) > /dev/null`
+    end
+    
+  end
+end
+
+TextMate::RVM.new.choose!
